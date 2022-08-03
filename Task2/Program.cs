e2605457c9c200845382bbbc3beff94fe58463f9
Ali <lisalmanov84@gmail.com>
2022-08-03 18:40:08 +0400

    hoomework

---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Salam";
+            int count = 0;
+            for (int i = 0; i < str .Length; i++)
+            {
+                if (str[i] == 'a')
+                {  
+                  count++;
+                }
+                   
+                    Console.WriteLine(count);
+            }
+        }
+    }
+}
